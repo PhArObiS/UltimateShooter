9fb2fdcd7e266e9115835067cd4389b0fa5f3b0d
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-04-30 19:52:18 -0400

    Added Binding Axis
    Addedd MoveForward, MoveRight, TurnRate, LookUpRate binds in ShooterCharacter class

--- Source/UltimateShooter/ShooterCharacter.cpp
+++ Source/UltimateShooter/ShooterCharacter.cpp
@@ -7,7 +7,9 @@
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
-AShooterCharacter::AShooterCharacter()
+AShooterCharacter::AShooterCharacter() :
+	BaseTurnRate(45.f),
+	BaseLookUpRate(45.f)
 {
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -30,15 +32,61 @@ void AShooterCharacter::BeginPlay()
 	
 }
 
-void AShooterCharacter::Tick(float DeltaTime)
+void AShooterCharacter::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.f))
+	{
+		// Find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		
+		// Get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AShooterCharacter::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.f))
+	{
+		// Find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		
+		// Get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+
+void AShooterCharacter::TurnRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds()); // deg/sec * sec/frame
+}
+
+void AShooterCharacter::LookUpRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds()); // deg/sec * sec/frame
+}
+
+void AShooterCharacter::Tick(float DeltaTime) 
 {
 	Super::Tick(DeltaTime);
 
 }
 
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
+{ 
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AShooterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AShooterCharacter::TurnRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AShooterCharacter::LookUpRate);
+    
+	
 }
-
+ 
--- Source/UltimateShooter/ShooterCharacter.h
+++ Source/UltimateShooter/ShooterCharacter.h
@@ -17,7 +17,39 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
+	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	/** Called for forwards/backwards input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/**
+	* Called via input to turn at a given rate.
+	* @param Rate  This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	*/
+	void TurnRate(float Rate);
+
+	/**
+	* Called via input to look up/down at a given rate.
+	* @param Rate  This is a normalized rate, i.e. 1.0 means 100% of desired rate
+	*/
+	void LookUpRate(float Rate);
+
+	/**
+	* Rotate controller based on mouse X movement
+	* @param Value   The input value from mouse movement
+	*/
+	// void Turn(float Value);
+
+	/**
+	* Rotate controller based on mouse Y movement
+	* @param Value   The input value from mouse movement
+	*/
+	// void LookUp(float Value);
+
 
 private:
 	/** Camera boom positioning the camera behind the character */
@@ -26,6 +58,12 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"));
 	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"));
+	float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"));
+	float BaseLookUpRate;
 
 public:
 	/** Returns CameraBoom subobject **/
