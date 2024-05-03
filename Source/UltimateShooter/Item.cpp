6ad6a95dda8f4f0942eab2b236a69e3597aed027
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-05-03 15:04:14 -0400

    Added Pickup Widget for weapons.
    Customized a weapon widget to show amount of ammo, which weapon, rarity stars, its name.

---
+++ Source/UltimateShooter/Item.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+#include "Components/BoxComponent.h"
+#include "Components/WidgetComponent.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ItemMesh"));
+	SetRootComponent(ItemMesh);
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBox->SetupAttachment(ItemMesh);
+
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(GetRootComponent());
+
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Hide Pickup Widget
+	PickupWidget->SetVisibility(false);
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+ 
+}
+
---
+++ Source/UltimateShooter/Item.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class ULTIMATESHOOTER_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItem();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	/** Skeletal Mesh for the item */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* ItemMesh;
+
+	/** Collision box for the item */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* CollisionBox;
+
+	/** Widget for the item */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UWidgetComponent* PickupWidget;
+
+	
+	
+public:	
+	
+
+};
