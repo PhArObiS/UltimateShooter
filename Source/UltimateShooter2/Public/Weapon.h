3b25e2893725f22b086c0c35718f8f329205439d
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-09-11 02:03:57 -0400

    Ultimate Shooter Upload
    Initial commit

---
+++ Source/UltimateShooter2/Public/Weapon.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULTIMATESHOOTER2_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+	
+public:
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+  
+protected:
+	void StopFalling();
+private:
+	FTimerHandle ThrowWeaponTimer;
+	float ThrowWeaponTime;
+	bool bFalling;
+public:
+	/** Adds and impulse to the weapon */
+	void ThrowWeapon();
+	
+};
---
+++ Source/UltimateShooter2/UltimateShooter2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UltimateShooter2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UltimateShooter2, "UltimateShooter2" );
